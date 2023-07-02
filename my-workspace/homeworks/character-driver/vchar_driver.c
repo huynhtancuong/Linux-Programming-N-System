edbdeca4ee7744732518233fa465e97b065fbcad
huynhtancuong <huynhtancuong.hcm@gmail.com>
2023-07-02 12:05:29 +0300

    Charater Device Driver: Create class and create driver

--- my-workspace/homeworks/character-driver/vchar_driver.c
+++ my-workspace/homeworks/character-driver/vchar_driver.c
@@ -1,12 +1,19 @@
 #include <linux/module.h> /* thu vien nay dinh nghia cac macro nhu module_init va module_exit */
 #include <linux/fs.h> /* thư viện này định nghĩa các hàm cấp phát/giải phóng device number */
+#include <linux/device.h> /* thư viện này chứa các hàm phục vụ cho việc tạo device file */
 
 #define DRIVER_AUTHOR "Huynh Tan Cuong"
 #define DRIVER_DESC   "A sample character device driver"
-#define DRIVER_VERSION "0.2"
+#define DRIVER_VERSION "0.4"
 
+/// @brief struct của character driver
+/**
+ * dev_class và dev là để phục vụ cho việc lưu kết quả trả về của hàm class_create và device_create
+*/
 struct _vchar_drv {
 	dev_t dev_num;
+	struct class *dev_class;
+	struct device *dev; 
 } vchar_drv;
 
 /****************************** device specific - START *****************************/
@@ -36,7 +43,7 @@ static int __init vchar_driver_init(void)
 
 	/* cap phat device number */
 	vchar_drv.dev_num = 0;
-	ret = alloc_chrdev_region(vchar_drv.dev_num, 1, "vchar_drv");
+	ret = alloc_chrdev_region(&vchar_drv.dev_num, 0, 1, "vchar_drv");
 	if (ret < 0) {
 		printk("Failed to register device number dynamically\n");
 		goto failed_register_devnum;
@@ -44,6 +51,27 @@ static int __init vchar_driver_init(void)
 	printk("Allocated device number dynamically. Device number is (%d,%d)\n", MAJOR(vchar_drv.dev_num), MINOR(vchar_drv.dev_num));
 
 	/* tao device file */
+		/**
+		 * Tạo ra một thiết bị có tên là "class_vchar_dev".
+		 * Nếu hàm này không thực hiện thành công, ta cần gọi hàm unregister_chrdev_region 
+		 * để giải phóng device number đã được cấp phát trước đó.
+		*/
+	vchar_drv.dev_class = class_create(THIS_MODULE, "class_vchar_dev");
+	if (vchar_drv.dev_class == NULL) {
+		printk("Failed to create device class\n");
+		goto failed_create_class;
+	}
+		/**
+		 * Nếu tạo class thành công, ta tiếp tục gọi hàm device_create để tạo ra một thiết bị có tên là "vchar_dev".
+		 * Thiết bị này nằm trong thư mục /dev.
+		 * Trong trường hợp hàm device_create không thực hiện được, ta cần gọi hàm destroy_class để giải phóng 
+		 * lớp thiết bị đã được tạo ra trước đó.
+		*/
+	vchar_drv.dev = device_create(vchar_drv.dev_class, NULL, vchar_drv.dev_num, NULL, "vchar_dev");
+	if (vchar_drv.dev == NULL) {
+		printk("Failed to create device\n");
+		goto failed_create_device;
+	}
 
 	/* cap phat bo nho cho cac cau truc du lieu cua driver va khoi tao */
 
@@ -56,6 +84,10 @@ static int __init vchar_driver_init(void)
 	printk("Initialize vchar driver successfully\n");
 	return 0;
 
+failed_create_device:
+	class_destroy(vchar_drv.dev_class);
+failed_create_class:
+	unregister_chrdev_region(vchar_drv.dev_num, 1);
 failed_register_devnum:
 	return ret;
 }
@@ -72,6 +104,8 @@ static void __exit vchar_driver_exit(void)
 	/* giai phong bo nho da cap phat cau truc du lieu cua driver */
 
 	/* xoa bo device file */
+	device_destroy(vchar_drv.dev_class, vchar_drv.dev_num);
+	class_destroy(vchar_drv.dev_class);
 
 	/* giai phong device number */
 	unregister_chrdev_region(vchar_drv.dev_num, 1);
